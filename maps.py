06178dc6a4b3524a90e6fee4ecfdacc588618efc
Alexander Hörnlein <aha5811@users.noreply.github.com>
2025-03-18 13:11:45 +0100

    cleanup maps

--- maps.py
+++ maps.py
@@ -1,53 +1,5 @@
 import utils
 
-d90 = [(1, 0), (0, 1), (-1, 0), (0, -1)] # W S E N
-dE, dS, dW,dN = d90[0], d90[1], d90[2], d90[3]
-d45 = [(1, 1), (-1, 1), (-1, -1), (1, -1)] # SE SW NW NE
-dSE, dSW, dNW, dNE = d45[0], d45[1], d45[2], d45[3]
-ds = d90 + d45
-
-class Map:
-    def from_file(fname):
-        return Map(utils.f2lines(fname))
-
-    def __init__(self, lines):
-        self.rows = []
-        for line in lines:
-            self.rows.append(list(line.strip()))
-        self.h = len(self.rows)
-        self.w = len(self.rows[0])
-
-    def __str__(self):
-        res = ""
-        for row in self.rows:
-            res += "".join(row) + "\n"
-        return res
-
-    def get(self, x, y):
-        if x < 0 or x >= self.w or y < 0 or y >= self.h:
-            return None
-        else:
-            return self.rows[y][x]
-
-    def set(self, x, y, c):
-        if self.get(x, y) is not None:
-            self.rows[y][x] = c
-
-    def get_symbols(self):
-        res = set(())
-        for x in range(self.w):
-            for y in range(self.h):
-                res.add(self.get(x, y))
-        return res
-
-    def find_all(self, s):
-        res = []
-        for x in range(self.w):
-            for y in range(self.h):
-                if self.get(x, y) == s:
-                    res.append(Pos(x, y))
-        return res
-
 class Pos:
     def __init__(self, x, y):
         self.x = x
@@ -63,3 +15,44 @@ class Pos:
 
     def __hash__(self):
         return hash(str(self))
+
+class Map:
+    def from_file(fname):
+        """ returns Map """
+        return Map(utils.f2lines(fname))
+
+    def __init__(self, lines):
+        self.rows = [list(line.strip()) for line in lines]
+        self.h: int = len(self.rows)
+        self.w: int = len(self.rows[0])
+
+    def __str__(self):
+        res = ''
+        for row in self.rows:
+            res += '\n' + ''.join(row)
+        return res[1:]
+
+    def get(self, x: int, y: int) -> str:
+        return (
+            None
+            if x < 0 or x >= self.w or y < 0 or y >= self.h
+            else self.rows[y][x])
+
+    def set(self, x: int, y: int, c: str):
+        if self.get(x, y) is not None:
+            self.rows[y][x] = c
+
+    def get_symbols(self) -> list[str]:
+        res = set()
+        for x in range(self.w):
+            for y in range(self.h):
+                res.add(self.get(x, y))
+        return list(res)
+
+    def find_all(self, s: str) -> list[Pos]:
+        res = []
+        for x in range(self.w):
+            for y in range(self.h):
+                if self.get(x, y) == s:
+                    res.append(Pos(x, y))
+        return res
